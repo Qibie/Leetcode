5a938f06633f1abad09551ced3a364939813aada
Qibie <35823703+Qibie@users.noreply.github.com>
2018-05-26 15:44:58 +0800

    Update and rename TwoSum Improved.java to TwoSumImproved.java

--- TwoSum/TwoSumImproved.java
+++ TwoSum/TwoSumImproved.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * @date 2018-04-28
  */
  
-class Solution {
+class TwoSumImproved {
 	public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
